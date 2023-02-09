3c784d6ff99886c0ccd71171374bdc6b901ab2c0
Wang Kai <18964368607@189.cn>
2023-02-09 21:37:03 +0800

    add -s option

--- pads.py
+++ pads.py
@@ -1,19 +1,134 @@
 from win32com import client as pads_client
 import argparse, re, sys
+import mysql.connector 
 
-about_info = 'tools for pads schematic, developed by Wang Kai, ver:0.1'
+db_access_config = {
+    'host':'192.168.0.100',
+    'user':'wangkai',
+    'password':'xxxxxx',
+    'database':'db_name',
+}
+about_info = 'tools for pads schematic, developed by Wang Kai, ver:0.2'
 valid_attrs_name = ('PART NO', 'Description', 'Geometry.Height', 'Manufacture 1', 'Manufacture 1 P/N', 'Not_In_Bom', 'Value') 
+
+errorPartsInfo = []
+def get_part_attributes_from_database(db_cnx, part_no):
+    part_with_part_no_attributes = {
+                    'PART_NO': '',
+                    'Description': '',
+                    'Manufacture_PN': '',
+                    'Manufacture': '',
+                    }
+    
+    try:
+        db_cursorA = db_cnx.cursor(buffered=True)
+        db_cursorB = db_cnx.cursor(buffered=True)
+
+        # for part in bomPartsList:                        
+        strQuery = 'SELECT part_desc, manufacture, order_partNo FROM wuliao_info WHERE sap_part_no=' + "'" + part_no + "'" + 'LIMIT 1'
+        query = (strQuery)
+        db_cursorA.execute(query,)
+        partCount = db_cursorA.rowcount               
+        db_cursor = db_cursorA
+        if partCount == 0:
+            strQuery = 'SELECT sap_desc, manufacture, mpn_desc FROM sap_mpn_map WHERE sap_part_no=' + "'" + part_no + "'" + 'LIMIT 1'
+            query = (strQuery)
+            db_cursorB.execute(query,)
+            partCount = db_cursorA.rowcount
+            db_cursor = db_cursorB
+            partCountB = db_cursorB.rowcount                    
+            if partCountB == 0:
+                errorPartsInfo.append(part_no)    
+                return None       
+        for item in db_cursor:
+            part_with_part_no_attributes = {
+                    'PART_NO': part_no,
+                    'Description': item[0],
+                    'Manufacture': item[1],
+                    'Manufacture_PN': item[2],                    
+                    }     
+            break          
+
+    except Exception as err_db_access:            
+            print(f'get_part_attributes_from_database error{str(err_db_access)}')
+
+    db_cursorA.close()
+    db_cursorB.close()
+    
+    return part_with_part_no_attributes
+
+def supplement_parts_attributes_info(powerlogic_app):
+    try:
+        db_cnx = mysql.connector.connect(**db_access_config)
+    except Exception as err:
+        print(f'get_part_attributes_from_database error{str(err)}')
+    powerlogic_app.LockServer
+    # part_type_with_part_info_list = []
+    # part_type_with_part_no_attributes = {'part_type':'', 'PART_NO':'', 'Description':'', 'Geometry_Height':'', 'Manufacture':'', 'Manufacture_PN':'', 'Value':''}
+    logic_prj = powerlogic_app.ActiveDocument
+    prj_part_types = logic_prj.PartTypes
+    print(f'prj part types:{prj_part_types.Count}')
+    prj_part_types.Sort()
+    total_part_types = prj_part_types.Count
+    deal_counter = 0
+    for j in range(1, prj_part_types.Count + 1):
+        part_type_with_part_no_attributes = {'part_type':'', 'PART_NO':'', 'Description':'', 'Geometry_Height':'', 'Manufacture':'', 'Manufacture_PN':'', 'Value':''}
+        part_type_name = prj_part_types.Item(j).Name
+        part_type_with_part_no_attributes['part_type'] = part_type_name        
+        comp = prj_part_types.Item(j).Components.Item(1)
+        # print(f'comp name:{comp.Name}')
+        # print(comp.Attributes.Item("PART NO").Value)
+        part_no = comp.Attributes.Item("PART NO").Value
+        comp_part_no_length = len(part_no)
+        # comp_attr_no_in_bom = comp.Attributes.Item("Not_In_Bom").Value
+        if part_no != "NC" and comp_part_no_length == 8:        
+            part_attributes = get_part_attributes_from_database(db_cnx=db_cnx, part_no=part_no)
+            if part_attributes is None:
+                print(f'\nget attributes from database error, part type:{part_type_name}')
+                continue
+            part_type_with_part_no_attributes['PART_NO'] = part_no
+            part_type_with_part_no_attributes['Description'] = part_attributes['Description']
+            part_type_with_part_no_attributes['Manufacture'] = part_attributes['Manufacture']
+            part_type_with_part_no_attributes['Manufacture_PN'] = part_attributes['Manufacture_PN']
+            # part_type_with_part_info_list.append(part_type_with_part_no_attributes)
+            comps_with_same_part_type = prj_part_types.Item(j).Components
+            for comp_with_same_part_type in comps_with_same_part_type:
+                comp_with_same_part_type.Attributes.Item("Description").Value = part_attributes['Description']
+                comp_with_same_part_type.Attributes.Item("Manufacture 1").Value = part_attributes['Manufacture']
+                comp_with_same_part_type.Attributes.Item("Manufacture 1 P/N").Value = part_attributes['Manufacture_PN']
+        else:
+            print(f'\nhave part with invalid part no while supplementing parts attributes, part no:{part_no}, part type:{part_type_name}')
+        deal_counter = deal_counter + 1
+        print(f'\rtotal part types:{total_part_types},deal counter:{deal_counter}', end='')
+    # print(part_type_with_part_info_list)
+    # print(f'part_type_list length:{len(part_type_list)}')
+    # print(f'selected part type is:{prj_part_types.Item(1).Name}')
+    powerlogic_app.UnlockServer
+    db_cnx.close()
+    print(f'\nerror parts info while access database{errorPartsInfo}')
+    print('supplement parts attributes info complete')
 
 def clean_parts_attributes(powerlogic_app):    
     powerlogic_app.LockServer
     logic_prj = powerlogic_app.ActiveDocument 
-    prj_comps = logic_prj.Components
+    
+    # collect part types in this prj
     part_type_list = []
+    prj_part_types = logic_prj.PartTypes
+    prj_part_types.Sort()
+    for j in range(1, prj_part_types.Count + 1):
+        part_type_list.append(prj_part_types.Item(j).Name)
+    print(f'all part types in this prj({len(part_type_list)}):\n{part_type_list}')
+    prj_comps = logic_prj.Components
+
+    total_prj_comps = prj_comps.Count
+    print(f'total components:{total_prj_comps}')
+    clean_counter = 0    
     for prj_comp in prj_comps:    
         # collect part types in this prj
-        comp_part_type = prj_comp.PartTypeObject
-        if comp_part_type.Name not in part_type_list:
-            part_type_list.append(comp_part_type.Name)
+        # comp_part_type = prj_comp.PartTypeObject
+        # if comp_part_type.Name not in part_type_list:
+        #     part_type_list.append(comp_part_type.Name)
         
         # checkt part attributes
         comp_attrs = prj_comp.Attributes
@@ -41,9 +156,13 @@ def clean_parts_attributes(powerlogic_app):
         for comp_gate in comp_gates:
             comp_gate.SetVisibility(0, 'Value', 1)
             comp_gate.SetVisibility(3, '', 0)
+
+        clean_counter = clean_counter + 1
+        print(f'\rclean counter:{clean_counter}', end='')
+        
     powerlogic_app.UnlockServer
-    print(f'all part types in this prj({len(part_type_list)}):\n{part_type_list}')
-    print('clean part attributes complete')
+    
+    print('\nclean part attributes complete')
 
 def check_parts_attributes(powerlogic_app):
     powerlogic_app.LockServer
@@ -51,6 +170,9 @@ def check_parts_attributes(powerlogic_app):
     prj_comps = logic_prj.Components
     comps_with_invalid_part_no = []
     part_type_with_invalid_part_no = []
+    total_prj_comps = prj_comps.Count
+    print(f'total components:{total_prj_comps}')
+    check_counter = 0 
     for prj_comp in prj_comps:            
         # check part attributes
         comp_attrs = prj_comp.Attributes
@@ -63,10 +185,15 @@ def check_parts_attributes(powerlogic_app):
             comp_part_type = prj_comp.PartTypeObject
             if comp_part_type.Name not in part_type_with_invalid_part_no:
                 part_type_with_invalid_part_no.append(comp_part_type.Name)
+        check_counter = check_counter + 1
+        print(f'\rcheck counter:{check_counter}', end='')
+
     powerlogic_app.UnlockServer
     print(f'\ncomponents with invalid part no({len(comps_with_invalid_part_no)}):\n{comps_with_invalid_part_no}\n')
-    print(f'part type with invalid part no({len(part_type_with_invalid_part_no)}):\n{part_type_with_invalid_part_no}')
+    part_type_with_invalid_part_no_count = len(part_type_with_invalid_part_no)
+    print(f'part type with invalid part no({part_type_with_invalid_part_no_count}):\n{part_type_with_invalid_part_no}')
     print('check part attributes complete')
+    return part_type_with_invalid_part_no_count
 
 def output_partlist():
     pass
@@ -75,13 +202,12 @@ if __name__ == '__main__':
     args_parser = argparse.ArgumentParser(prog='pads tools', description='check Schematic and Part Attributes, output partlist to excel for check')
     args_parser.add_argument('projectName', help='project name to deal with')
     args_parser.add_argument('--about', help=about_info, action='store_true')
-    args_parser.add_argument('-a','--attr', help='clean parts attributes of opening schematic', action='store_true')
-    args_parser.add_argument('-c','--check', help='check parts attributes of opening schematic', action='store_true')
+    args_parser.add_argument('-a','--attr', help='clean parts attributes of opening schematic(将不使用的属性字段从原理图中删除)', action='store_true')
+    args_parser.add_argument('-c','--check', help='check parts attributes information of opening schematic(检查原理图中所有器件是否分配了PART NO)', action='store_true')
+    args_parser.add_argument('-s','--supplement', help='supplement parts attributes information of opening schematic(根据提供的PART NO信息，从数据库检索该器件的属性信息，并添加到每个器件里)', action='store_true')
     args_parser.add_argument('-p','--partlist', help='output parts list to excel', action='store_true')
     user_args = args_parser.parse_args()
-    # args_dict = vars(user_args)
-    # print(args_dict)
-    
+        
     logic_app = pads_client.Dispatch('PowerLogic.Application')
     
     print(logic_app.Name)
@@ -103,6 +229,10 @@ if __name__ == '__main__':
         print('check parts attributes of opening schematic')
         check_parts_attributes(logic_app)
         sys.exit(0)
+    if user_args.supplement:
+        print('supplement parts attributes info')
+        supplement_parts_attributes_info(logic_app)
+        sys.exit(0) 
     if user_args.partlist:
         print('output parts list to excel')
         sys.exit(0)        
--- pads_function_test.py
+++ pads_function_test.py
@@ -1,26 +1,118 @@
 from win32com import client as pads_client
 import argparse
 import re, sys
+import mysql.connector 
 
 part_index  = 51
+db_access_config = {
+    'host':'192.168.0.100',
+    'user':'wangkai',
+    'password':'xxxxxx',
+    'database':'db_name',
+}
+errorPartsInfo = []
+def get_part_attributes_from_database(db_cnx, part_no):
+    part_with_part_no_attributes = {
+                    'PART_NO': '',
+                    'Description': '',
+                    'Manufacture_PN': '',
+                    'Manufacture': '',
+                    }
+    
+    try:
+        db_cursorA = db_cnx.cursor(buffered=True)
+        db_cursorB = db_cnx.cursor(buffered=True)
 
-part_type_list = []
-parts_list_with_same_part_type = []
+        # for part in bomPartsList:                        
+        strQuery = 'SELECT part_desc, manufacture, order_partNo FROM wuliao_info WHERE sap_part_no=' + "'" + part_no + "'" + 'LIMIT 1'
+        query = (strQuery)
+        db_cursorA.execute(query,)
+        partCount = db_cursorA.rowcount               
+        db_cursor = db_cursorA
+        if partCount == 0:
+            strQuery = 'SELECT sap_desc, manufacture, mpn_desc FROM sap_mpn_map WHERE sap_part_no=' + "'" + part_no + "'" + 'LIMIT 1'
+            query = (strQuery)
+            db_cursorB.execute(query,)
+            partCount = db_cursorA.rowcount
+            db_cursor = db_cursorB
+            partCountB = db_cursorB.rowcount                    
+            if partCountB == 0:
+                errorPartsInfo.append(part_no)    
+                return None       
+        for item in db_cursor:
+            part_with_part_no_attributes = {
+                    'PART_NO': part_no,
+                    'Description': item[0],
+                    'Manufacture': item[1],
+                    'Manufacture_PN': item[2],                    
+                    }     
+            break          
+
+    except Exception as err_db_access:            
+            print(f'get_part_attributes_from_database error{str(err_db_access)}')
+
+    db_cursorA.close()
+    db_cursorB.close()
+    
+    return part_with_part_no_attributes
+
+def supplement_parts_attributes_info(powerlogic_app):
+    try:
+        db_cnx = mysql.connector.connect(**db_access_config)
+    except Exception as err:
+        print(f'get_part_attributes_from_database error{str(err)}')
+    powerlogic_app.LockServer
+    part_type_with_part_info_list = []
+    # part_type_with_part_no_attributes = {'part_type':'', 'PART_NO':'', 'Description':'', 'Geometry_Height':'', 'Manufacture':'', 'Manufacture_PN':'', 'Value':''}
+    logic_prj = powerlogic_app.ActiveDocument
+    prj_part_types = logic_prj.PartTypes
+    print(f'prj part types:{prj_part_types.Count}')
+    prj_part_types.Sort()
+    for j in range(1, prj_part_types.Count + 1):
+        part_type_with_part_no_attributes = {'part_type':'', 'PART_NO':'', 'Description':'', 'Geometry_Height':'', 'Manufacture':'', 'Manufacture_PN':'', 'Value':''}
+        part_type_name = prj_part_types.Item(j).Name
+        part_type_with_part_no_attributes['part_type'] = part_type_name        
+        comp = prj_part_types.Item(j).Components.Item(1)
+        # print(f'comp name:{comp.Name}')
+        # print(comp.Attributes.Item("PART NO").Value)
+        part_no = comp.Attributes.Item("PART NO").Value
+        comp_part_no_length = len(part_no)
+        # comp_attr_no_in_bom = comp.Attributes.Item("Not_In_Bom").Value
+        if part_no != "NC" and comp_part_no_length == 8:        
+            part_attributes = get_part_attributes_from_database(db_cnx=db_cnx, part_no=part_no)
+            if part_attributes is None:
+                print(f'get attributes from database error, part type:{part_type_name}')
+                continue
+            part_type_with_part_no_attributes['PART_NO'] = part_no
+            part_type_with_part_no_attributes['Description'] = part_attributes['Description']
+            part_type_with_part_no_attributes['Manufacture'] = part_attributes['Manufacture']
+            part_type_with_part_no_attributes['Manufacture_PN'] = part_attributes['Manufacture_PN']
+            part_type_with_part_info_list.append(part_type_with_part_no_attributes)
+            comps_with_same_part_type = prj_part_types.Item(j).Components
+            for comp_with_same_part_type in comps_with_same_part_type:
+                comp_with_same_part_type.Attributes.Item("Description").Value = part_attributes['Description']
+                comp_with_same_part_type.Attributes.Item("Manufacture 1").Value = part_attributes['Manufacture']
+                comp_with_same_part_type.Attributes.Item("Manufacture 1 P/N").Value = part_attributes['Manufacture_PN']
+        else:
+            print(f'have part with invalid part no while supplementing parts attributes, part no:{part_no}, part type:{part_type_name}')
+    print(part_type_with_part_info_list)
+    # print(f'part_type_list length:{len(part_type_list)}')
+    # print(f'selected part type is:{prj_part_types.Item(1).Name}')
+    powerlogic_app.UnlockServer
+    db_cnx.close()
+    print(f'error parts info while access database{errorPartsInfo}')
+    print('supplement_parts_attributes_info complete')
+
+# parts_list_with_same_part_type = []
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='check Schematic and Part Attributes, output partlist to excel for check')
-    parser.add_argument('projectName', help='project name to deal with')
-    parser.add_argument('-c','--check', help='check parts attributes of opening schematic', action='store_true')
-    parser.add_argument('-p','--partlist', help='output parts list to excel', action='store_true')
-    args = parser.parse_args()
-    args_dict = vars(args)
-    print(args_dict)
-    if args.check:
-        print('checkt schematic')
-    if args.partlist:
-        print('output parts list to excel')    
-
+    args_parser = argparse.ArgumentParser(description='check Schematic and Part Attributes, output partlist to excel for check')
+    args_parser.add_argument('projectName', help='project name to deal with')
+    args_parser.add_argument('-c','--check', help='check parts attributes of opening schematic', action='store_true')
+    args_parser.add_argument('-s','--supplement', help='supplement parts attributes information of opening schematic', action='store_true')
+    args_parser.add_argument('-p','--partlist', help='output parts list to excel', action='store_true')
+    args = args_parser.parse_args()
+        
     logic_app = pads_client.Dispatch('PowerLogic.Application')    
-    # logic_app = pads_client.Dispatch('PowerPCB.Application')
     print(logic_app.Name)
     logic_prj = logic_app.ActiveDocument 
     print(logic_prj.Name)
@@ -30,37 +122,42 @@ if __name__ == '__main__':
         print(print('project name invalid'))
         sys.exit(0)
     
-    print(f'project name valid:{match_result.group()}')  
-        
-    logic_app.LockServer
-    prj_comps = logic_prj.Components
+    print(f'project name valid:{match_result.group()}')    
+    if args.check:
+        print('checkt schematic')
+    if args.supplement:
+        print('supplement_parts_attributes_info')
+        # sap = '10000120'
+        # strQuery = 'SELECT part_desc, manufacture, order_partNo FROM wuliao_info WHERE sap_part_no=' + "'" + sap + "'"
+        # print(strQuery)
+        supplement_parts_attributes_info(logic_app)
     # comps_part_types = logic_prj.AttributeTypes
     # print(comps_part_types.Count)
     # for i in range(1, comps_part_types.Count +1):
     #     print(comps_part_types.Item(i))
-    comp_part_type = prj_comps(part_index).PartTypeObject
-    comp_part_name = prj_comps(part_index).Name
-    comp_gates = prj_comps(part_index).Gates
+    # comp_part_type = prj_comps(part_index).PartTypeObject
+    # comp_part_name = prj_comps(part_index).Name
+    # comp_gates = prj_comps(part_index).Gates
     # print(comp_part_name)
-    print(comp_part_type.Name)
-    comps_this_part_type = comp_part_type.Components
-    print(f'comps_this_part_type:{comps_this_part_type.Count}')
-    for i in range(1, comps_this_part_type.Count + 1):
+    # print(comp_part_type.Name)
+    # comps_this_part_type = comp_part_type.Components
+    # print(f'comps_this_part_type:{comps_this_part_type.Count}')
+    # for i in range(1, comps_this_part_type.Count + 1):
         # print(comps_this_part_type.Item(i).Name)
-        parts_list_with_same_part_type.append(comps_this_part_type.Item(i).Name)
+        # parts_list_with_same_part_type.append(comps_this_part_type.Item(i).Name)
     # print(comp_gates.Count)
     # print(comp_gates.Item(1).GetVisibility(0, 'Value'))
     # for comp_gate in comp_gates:
     #     comp_gate.SetVisibility(3, '', 1)        
     #     print(comp_gate.GetVisibility(0, 'PART NO'))
-    print(parts_list_with_same_part_type)
-    logic_app.UnlockServer
+    # print(parts_list_with_same_part_type)
+    
     print('complete')
 
     # part_no = prj_comp.Attributes.Item("PART NO")
         # # print(part_no)
         # part_no_str = str(part_no)
-        # if part_no_str == '10025376':
+        # if part_no_str == '11125376':
         #     print(prj_comp.name)
         #     print(prj_comp.Attributes.Item("PART NO"))
         #     print(prj_comp.Attributes.Item("Value"))    
@@ -78,7 +175,7 @@ if __name__ == '__main__':
 # print(pcb_prj.name)
 # pcp_comps = pcb_prj.components
 # for pcb_comp in pcp_comps:    
-#     if pcb_comp.Attributes.Item("PART NO") == '10024519':
+#     if pcb_comp.Attributes.Item("PART NO") == '11124519':
 #         print(pcb_comp.name)
 #         print(pcb_comp.Attributes.Item("PART NO"))
 #         print(pcb_comp.Attributes.Item("Value"))    
